1b4ccc4e98c93c368870c56a3fb311e280c3893d
Jaden Ong <ong.jadengabriel@gmail.com>
2025-09-05 22:12:12 -0700

    initial spring boot project setup

---
+++ src/main/java/com/algocoach/AlgoCoachApplication.java
@@ -0,0 +1,12 @@
+package com.algocoach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgoCoachApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgoCoachApplication.class, args);
+    }
+}
