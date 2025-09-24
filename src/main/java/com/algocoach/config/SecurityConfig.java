f210cde9bcbddc8ee7b9c832ba454b8b785eb4a3
Jaden Ong <ong.jadengabriel@gmail.com>
2025-09-23 21:05:15 -0700

    Update SecurityConfig.java

--- src/main/java/com/algocoach/config/SecurityConfig.java
+++ src/main/java/com/algocoach/config/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
+        // BCrypt strength of 12 provides good security with reasonable performance
         return new BCryptPasswordEncoder(12);
     }
 
